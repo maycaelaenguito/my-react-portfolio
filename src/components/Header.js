73dd5700532fb3d6bd1e0fd7de5f2f5da8161aac
maycaelaenguito <maycaelaenguito@gmail.com>
2023-04-03 06:28:27 -0400

    initial commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
   return <header className="py-8">
     <div className="container mx-auto">
       <div className="flex justify-between items-center">
-        <a href="#" className="logo container mx-auto">
+        <a href="##" className="logo container mx-auto">
           <img src={Logo} alt="logo" className='img-fluid'/>
           </a>
       </div>
